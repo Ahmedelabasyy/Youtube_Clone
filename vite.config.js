dbc05731ab91890f2483f0c14647a7017fdee9bd
Ahmedelabasyy <ahmedelabasy662@gmail.com>
2023-02-12 22:41:33 +0200

    gh-pages prepare

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Youtube_Clone',
+  base: '/Youtube_Clone/',
   plugins: [react()]
 })
